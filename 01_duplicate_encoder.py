d62e1a63717c36efa51a24dfc765d98db45e68a0
perm <110578750+MugumoPerm@users.noreply.github.com>
2023-07-28 09:31:18 +0300

    python first commit 01_duplicate encoder

---
+++ 01_duplicate_encoder.py
@@ -0,0 +1,20 @@
+def duplicate_encode(word):
+    length = len(word)
+    left = 0
+    #right = length-1
+    
+    letter_count =  (word.count(word[left]))
+    for i in range(length):
+        
+    
+    
+        
+
+    
+    
+    
+    
+    
+word = "din"
+
+print(duplicate_encode(word))
