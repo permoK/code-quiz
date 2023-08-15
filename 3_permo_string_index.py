33c5b7cdf7ffd15ffccf5ce99064f37f91143b6d
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-15 23:46:20 +0300

    exercise 3 done

---
+++ 3_permo_string_index.py
@@ -0,0 +1,9 @@
+#function to print strings from their index
+def string_index(my_string):
+    #create a loop to iterate the string
+    for i in my_string:
+        #print the string
+        print(i)
+
+#call the function
+string_index('string')
