0059b9a3c440b870676e36479f230135126b015a
senrono <rono14065@student.mmarau.ac.ke>
2023-08-15 22:58:34 +0300

    Create calc.py
    @Sen added my Exercise 1

---
+++ calc.py
@@ -0,0 +1,22 @@
+# @ Sen Excercise 1
+
+#  Multiplication and sum
+
+# Taking user input
+num_1=int(input('Enter the first integer: '))
+num_2=int(input('Enter the second integer: '))
+
+# Doing the multiplication
+
+results=num_1*num_2
+
+# using if statement to check if the result <=1000
+if results<=1000:
+    print(f'\nThe results is {results}')
+    
+    print('\n-------------copyright 2023 --------------------\n')
+else:
+    # doing the summation
+    sum=num_1+num_2
+    print(f'\nThe results is {sum}')
+    print('\n-------------copyright 2023 --------------------\n')
