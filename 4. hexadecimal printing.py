776e7e6a9080302ebb28e30f0aaa08d35a01c01e
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-15 20:35:06 +0300

    add perm_calculator.py

---
+++ 4. hexadecimal printing.py
@@ -0,0 +1,4 @@
+#this program prints all numbers from 0 to 98 in decimal and in hexadecimal using for loop
+
+for i in range(100):
+    print("{0:id}".format(i))
---
+++ calculator.py
@@ -0,0 +1,18 @@
+#this function returns their product only if the product is equal to or lower than 1000, else return their sum.
+
+def cal():
+    first = input("Enter the first integer: ")
+    second = input("Enter the second integer: ")
+    #calculate the product
+    product = int(first) * int(second)
+
+    #check if the product is greater than 1000
+    if product > 1000:
+        #if greater make the sum of the integers
+        sum =  int(first) + int(second)
+        return sum
+    else: #if product is less than 1000 return the product instead
+        return product
+    
+#call the function
+print(cal())
