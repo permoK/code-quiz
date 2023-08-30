c5b130b2898d1ac3b8978c6909e45da15643d808
senrono <rono14065@student.mmarau.ac.ke>
2023-08-30 16:50:22 +0300

    Adding 7 8
    Exercise 7 and 8

---
+++ sen/7_sen_Stringcounter.py
@@ -0,0 +1,7 @@
+# ---------@sen-----------
+#Excercise 7
+# Count a string 
+
+str_x = "Emma is good developer. Emma is a writer Emma"
+
+print(f"Emma apperas {str_x.count('Emma')} times")
---
+++ sen/8_sen_pattern.py
@@ -0,0 +1,10 @@
+# --------- sen --------------
+# Exercise 8
+
+i=1
+j=1
+
+for k in range(1, 6):
+    print(str(i)*j)
+    i+=1
+    j+=1
