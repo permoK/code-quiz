c701c202f16a3c8cda2ddef2a059cd58c6cd0d6a
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-09-11 08:28:04 +0300

    update

--- permo/decorators2.py
+++ permo/decorators2.py
@@ -1,23 +1,24 @@
 #recap of decorators
 
-def trigger(f):
-    def permission():
+#the f in the decorator function replaces any function as used when calling the decorator
+
+def trigger(f): #3
+    def permission(): #4
         print("waits for the function")
 
         #this calls the function with the decorator
-        f()
+        f() #5
 
         print("actually calls the function")
+#this returns the function held by the decorator
+    return permission #4
 
-    return permission
 
-
-#add the decorator function
-@trigger 
-def hello(): #define a function 
+#add the decorator 
+@trigger  #2
+def hello(): #define a function  #5
     print("hello world")
 
 
 #call the hello function and see whether the decorator does the work
-
-hello()
+hello()  #1
